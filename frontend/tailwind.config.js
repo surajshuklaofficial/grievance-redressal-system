f16a57bdf76b366c68073691d3eb1753d59baa13
manthan23s <manthan2306s@gmail.com>
2023-09-14 22:37:44 +0530

    init  frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        sm: "0 1px 3px rgba(0, 0, 0, 0.1)",
+        md: "0 4px 6px rgba(0, 0, 0, 0.1)",
+        lg: "0 10px 15px rgba(0, 0, 0, 0.1)",
+        xl: "0 20px 25px rgba(0, 0, 0, 0.1)",
+      },
+    },
+  },
+  plugins: [],
+};
